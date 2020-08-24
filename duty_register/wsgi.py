aff9470467f2f9e42fd1bc3e5901927fdade148d
Jubril Aminu <jubrilaminu304@gmail.com>
2020-08-24 08:57:30 +0100

    resolved wsgi shit

--- duty_register/wsgi.py
+++ duty_register/wsgi.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'duty_register.settings.development')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
+# application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'duty_register/duty/static'))
