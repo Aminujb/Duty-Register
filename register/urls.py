2f47147d3b129b078a9530f3b64cba29bc876bfc
Jubril Aminu <jubrilaminu304@gmail.com>
2020-09-06 22:59:49 +0100

    added search feature for employees

--- register/urls.py
+++ register/urls.py
@@ -5,6 +5,7 @@ app_name = 'register'
 
 urlpatterns = [
     path('', views.index, name='employee_list'),
+    path('search/', views.SearchEmployeesView.as_view(), name='search_results'),
     path('employees/', views.EmployeesListView.as_view(), name='employee_list'),
     path('employee-detail/<int:pk>', views.EmployeeDetailView.as_view(), name='employee_detail'),
     path('employees/upload-records/', views.upload_records, name='upload_records'),
--- register/views.py
+++ register/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
@@ -19,6 +20,24 @@ class EmployeesListView(ListView):
 class EmployeeDetailView(DetailView):
     model = Employee
     template_name = 'register/employee_detail.html'
+
+
+class SearchEmployeesView(ListView):
+    model = Employee
+    template_name = 'register/employee_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchEmployeesView, self).get_context_data(**kwargs)
+        context["search_word"] = self.request.GET.get('q')
+        context["search_not_found"] = 'No such employee record available'
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Employee.objects.filter(
+            Q(last_name__icontains=query) | Q(first_name__icontains=query) | Q(middle_name__icontains=query)
+        )
+        return object_list
 
 
 def upload_records(request, next_item='register:employee_list'):
